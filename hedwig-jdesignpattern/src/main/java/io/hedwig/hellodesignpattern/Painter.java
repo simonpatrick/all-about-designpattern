fb8da3abaa46a308572eb00bc29855c6308f72c6
patrick <patrick@patrickdeMacBook-Pro.local>
2015-12-22 23:54:45 +0800

    update design pattern

--- hedwig-jdesignpattern/src/main/java/io/hedwig/hellodesignpattern/Painter.java
+++ hedwig-jdesignpattern/src/main/java/io/hedwig/hellodesignpattern/Painter.java
@@ -13,25 +13,26 @@ public class Painter {
 //    - get different package name for different classes
 //    - write to file and use command to generate png file
     public static void main(String[] args) throws IOException, ClassNotFoundException {
-       String path= Painter.class.getClassLoader().getResource("").getPath()+
-               "io/hedwig/hellodesignpattern";
-        String basePackageName ="io.hedwig.hellodesignpattern";
+        String path = Painter.class.getClassLoader().getResource("").getPath() +
+                "io/hedwig/hellodesignpattern";
+        String basePackageName = "io.hedwig.hellodesignpattern";
         System.out.println(path.equals(path));
         System.out.println(path);
         File file = new File(path);
-        if(file.isDirectory()){
-           File[] files = file.listFiles();
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
             for (File childFile : files) {
-                if(childFile.isDirectory()){
+                if (childFile.isDirectory()) {
                     String packageSimpleName = childFile.getName();
                     File[] fs = childFile.listFiles();
+                    assert fs != null;
                     for (File f : fs) {
-                        if(f.isDirectory()){
+                        if (f.isDirectory()) {
                             System.out.println(packageSimpleName);
                             System.out.println(f.getName());
-                            System.out.println(basePackageName + "." + packageSimpleName+"."+f.getName());
+                            System.out.println(basePackageName + "." + packageSimpleName + "." + f.getName());
                             DomainMapperPainter.build()
-                                    .addPackage(basePackageName + "." + packageSimpleName+"."+f.getName())
+                                    .addPackage(basePackageName + "." + packageSimpleName + "." + f.getName())
                                     .writeTo(f.getName() + ".dot").paint();
                         }
                     }
